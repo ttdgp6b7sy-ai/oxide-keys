2a663726107a9f7c95b1f65e43baccf0288f9b4a
Ren-Jopson <124571463+Ren-Jopson@users.noreply.github.com>
2026-09-04 05:30:12 +1000

    fix firmware build errors

--- firmware/build.rs
+++ firmware/build.rs
@@ -3,8 +3,9 @@ use std::fs::File;
 use std::io::Write;
 use std::path::PathBuf;
 
+// copy memory.x next to the build output so the linker finds it
 fn main() {
-    let out = &PathBuf::from(env::var_os("OUT_DIR").unwrap());
+    let out = PathBuf::from(env::var_os("OUT_DIR").unwrap());
     File::create(out.join("memory.x"))
         .unwrap()
         .write_all(include_bytes!("memory.x"))
